278246e0db47ea21a85244087c1495cf01bf0796
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-05 02:53:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
-def introduction_with_language("name", "language")
-  puts "Hi, my name is #{ralph} and I am learning to code in #{ruby}."
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction_with_language(name, language)
+ puts "Hi, my name is #{ralph} and I am learning to code in #{lanf}."
 end
